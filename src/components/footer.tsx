d48049a7ef9ad6e36675ca96fdd073dcb5b271dd
Ruel Mindo <wrelmind@gmail.com>
2023-06-17 19:13:55 +0800

    Changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <footer>
       <div className="inner">
-        <span>Copyright ©2023 Deziper. Design By Me.</span>
+        <span>Copyright ©2023 Deziper. Designed By Me.</span>
       </div>
     </footer>
   )
